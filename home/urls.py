a3967efb68239158bf05da5c009057474a1ea605
smalex-z <alexzheng2004@gmail.com>
2023-08-23 14:34:01 -0400

    database created + sort function added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("wardrobe/", views.wardrobe, name="wardrobe")
+    path("wardrobe/", views.wardrobe, name="wardrobe"),
 ]
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
@@ -6,4 +7,17 @@ def home(request):
     return render(request, 'home/home.html')    
 
 def wardrobe(request):
-    return render(request, 'home/wardrobe.html')
+    products = Product.objects.all()
+    sort_by = request.GET.get('sort')
+
+    if sort_by == 'price-asc':
+        products = products.order_by('price')
+    elif sort_by == 'price-desc':
+        products = products.order_by('-price')
+    elif sort_by == 'newest':
+        products = products.order_by('-date_added')
+
+    context = {'products': products}
+
+    return render(request, 'home/wardrobe.html', context)
+
