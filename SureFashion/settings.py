521e8ab21136a16169299486a1cc989c5684e64b
smalex-z <alexzheng2004@gmail.com>
2023-09-06 17:03:43 -0400

    got rid of error log

--- SureFashion/settings.py
+++ SureFashion/settings.py
@@ -135,30 +135,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'home_error.log',  # Specify your log file name
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'home': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
-
 # Social Authentication Settings
 AUTHENTICATION_BACKENDS = (
     # ... other authentication backends ...
