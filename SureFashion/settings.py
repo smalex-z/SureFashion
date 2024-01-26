b092e87dabf91d0fbbad1a92ab65a3b2ef99f77d
Alex Zheng <75999032+smalex-z@users.noreply.github.com>
2024-01-25 20:14:45 -0800

    redated

--- SureFashion/settings.py
+++ SureFashion/settings.py
@@ -142,7 +142,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '294634314827-rdeg2qk9sd5haishvd2m2qrrkmnf6uk3.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '' #redacted for public repo
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '' #redacted for public repo
 
 LOGIN_REDIRECT_URL = '/wardrobe/'  # Replace '/dashboard/' with your desired URL
